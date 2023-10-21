397cb7364f31bf27a73e12adc967aad169c3f9e4
Dmitrij <th3_fox@Nova.localdomain>
2023-10-21 23:39:26 +0300

    Add comments and file allocation

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/* The code is creating variables and selecting elements from the DOM using JavaScript. */
 const container = document.querySelector("div");
 const gridContainer = document.createElement("div");
 gridContainer.classList.add("grid-container");
@@ -17,12 +18,22 @@ const btns = document.querySelectorAll(".btn");
 let isRgb = false;
 let isColorPickerActive = false;
 
+/**
+ * The getRandomRgb function generates a random RGB color in the format "rgb(r, g, b)".
+ * @returns a randomly generated RGB color in the format "rgb(r, g, b)".
+ */
 function getRandomRgb() {
     randomByte = () => Math.floor(Math.random() * 256);
     const randomRgbColor = `rgb(${randomByte()}, ${randomByte()}, ${randomByte()})`;
     return randomRgbColor;
 }
 
+/**
+ * The function creates a grid of div elements with a specified size and adds event listeners to change
+ * the background color of each div when the mouse is over it.
+ * @param size - The `size` parameter represents the number of rows and columns in the grid. It
+ * determines the size of the grid.
+ */
 function createGrid(size) {
     const fragment = document.createDocumentFragment();
 
@@ -52,12 +63,17 @@ function createGrid(size) {
     gridContainer.appendChild(fragment);
 }
 
+/* The code is adding an event listener to the `slider` element. When the input value of the slider
+changes, the event listener function is triggered. */
 slider.addEventListener("input", () => {
     gridSize = slider.value;
     document.querySelector("#range").textContent = `${gridSize}x${gridSize}`;
     createGrid(gridSize);
 });
 
+/* The code is adding event listeners to each button in the `btns` NodeList. When a button is clicked,
+the event listener function is triggered. Inside the function, it checks the `id` of the clicked
+button using `btn.id`. Based on the `id`, it performs different actions: */
 btns.forEach((btn) => {
     btn.addEventListener("click", () => {
         const button = btn.id;
@@ -83,6 +99,10 @@ btns.forEach((btn) => {
     });
 });
 
+/**
+ * The clearGrid function clears the background color of all elements with the class "item" in the
+ * grid.
+ */
 function clearGrid() {
     const gridItems = document.querySelectorAll(".item");
     gridItems.forEach(item => {
